e0e58284261e745423954cc58582cf1754635423
VallejoAgustin <AgustinVallejo@my.unt.edu>
2019-02-26 16:14:56 -0600

    added email and connect activities and connected buttons

--- app/src/main/java/com/example/a911simulator/HomeActivity.java
+++ app/src/main/java/com/example/a911simulator/HomeActivity.java
@@ -1,5 +1,7 @@
 package com.example.a911simulator;
 
+import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,5 +13,45 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //grab buttons and put into variables
+        Button getStartedButton = findViewById(R.id.getStartedButton);
+        Button settingsButton = findViewById(R.id.settingsButton);
+        Button surveyButton = findViewById(R.id.surveyButton);
+
+        /*
+         * create listeners for starting, settings, and survey button
+         * each listener opens up a new activity
+         * */
+
+        getStartedButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //creates an intent for the ConnectActivity class
+                Intent connect = new Intent(HomeActivity.this, ConnectActivity.class);
+
+                startActivity(connect);
+            }
+        });
+
+        settingsButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //creates an intent for the SettingsActivity class
+                Intent settings = new Intent(HomeActivity.this, SettingsActivity.class);
+
+                startActivity(settings);
+            }
+        });
+
+        surveyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //creates an intent for the EmailActivity class
+                Intent email = new Intent(HomeActivity.this, EmailActivity.class);
+
+                startActivity(email);
+            }
+        });
     }
 }
