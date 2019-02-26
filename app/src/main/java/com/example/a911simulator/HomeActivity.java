bb9a8c8ee07ca4d238f30f167af385e19e54e8dd
VallejoAgustin <AgustinVallejo@my.unt.edu>
2019-02-26 15:57:39 -0600

    added custom button style throughout project and designed home screen

--- app/src/main/java/com/example/a911simulator/HomeActivity.java
+++ app/src/main/java/com/example/a911simulator/HomeActivity.java
@@ -2,6 +2,8 @@ package com.example.a911simulator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
