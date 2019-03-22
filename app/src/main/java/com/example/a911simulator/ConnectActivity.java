d6badd0e62be89ab206b8dfa1a2e16cba0741e94
Dakota Lambert <32375609+Dako7a@users.noreply.github.com>
2019-03-21 19:34:55 -0500

    Further progress made on the device connections
    Sockets created to send a message between devices upon connection.
    
    Removed a requirement in the manifest for the minimum SDK.
    
    TODO: Find a way to manually remove "remembered groups"

--- app/src/main/java/com/example/a911simulator/ConnectActivity.java
+++ app/src/main/java/com/example/a911simulator/ConnectActivity.java
@@ -10,6 +10,9 @@ import android.net.wifi.p2p.WifiP2pDeviceList;
 import android.net.wifi.p2p.WifiP2pGroup;
 import android.net.wifi.p2p.WifiP2pInfo;
 import android.net.wifi.p2p.WifiP2pManager;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,11 +20,18 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -34,6 +44,7 @@ public class ConnectActivity extends AppCompatActivity {
     TextView connectionStatus;
     TextView connectionStatus2;
     ListView listView;
+    Button btnSend;
     // END SECTION OF VARIABLES FOR BUTTON LISTENERS
 
 
@@ -51,6 +62,13 @@ public class ConnectActivity extends AppCompatActivity {
     WifiP2pDevice[] deviceArray; //the actual devices we find in our nearby enviornment.
     // END SECTION OF VARIABLES FOR DEVICE CONNECTION
 
+    static final int MESSAGE_READ = 1;
+
+    ServerClass serverClass;
+    ClientClass clientClass;
+    SendReceive sendReceive;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -66,6 +84,23 @@ public class ConnectActivity extends AppCompatActivity {
         }
         // END SECTION OF FUNCTIONS FOR DEVICE CONNECTION
     }
+
+    Handler handler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            switch(msg.what) //this is where the buffer actually sets to the screen.
+            {
+                case MESSAGE_READ:
+                    byte [] readBuff = (byte[]) msg.obj;
+                    String tempMsg = new String(readBuff, 0, msg.arg1);
+                    //ggread_msg_box.setText(tempMsg);
+                    Toast.makeText(getApplicationContext(), tempMsg, Toast.LENGTH_SHORT).show();
+                    break;
+            }
+            return true;
+        }
+    });
+
     private void exqListener() {
 //        Functionality for the "Discover" button.
         btnDiscover.setOnClickListener(new View.OnClickListener() {
@@ -80,6 +115,8 @@ public class ConnectActivity extends AppCompatActivity {
                     @Override //wifi turned off ( i did not get this to trigger with wifi turned on )
                     public void onFailure(int i) {
                         connectionStatus.setText("Discovery Starting Failed");
+                        wifiManager.setWifiEnabled(false);
+                        wifiManager.setWifiEnabled(true);
                     }
                 });
             }
@@ -136,6 +173,49 @@ public class ConnectActivity extends AppCompatActivity {
             }
         });
 
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String msg="test";
+                // sendReceive.write(msg.getBytes());
+                try {
+                    sendtask t1 = new sendtask(msg);
+                    t1.execute();
+                }catch (Exception  e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public class sendtask extends AsyncTask<Void, Void, Void> {
+
+        String message;
+
+
+        sendtask(String msg) {
+            message=msg;
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            try {
+                sendReceive.write(message.getBytes());
+            } catch (Exception e) { //if the button is pressed without a socket being created
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+
+            super.onPostExecute(result);
+
+        }
+
     }
 
     private void initialWork() { //initialization function called upon start up for the application
@@ -145,6 +225,7 @@ public class ConnectActivity extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.peerListView);
         connectionStatus = (TextView) findViewById(R.id.connectionStatus);
         connectionStatus2 = (TextView) findViewById(R.id.connectionStatus2);
+        btnSend = (Button) findViewById(R.id.sendButton);
 
         wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE); //does what?
 
@@ -200,12 +281,16 @@ public class ConnectActivity extends AppCompatActivity {
             final InetAddress groupOwnerAddress = wifiP2pInfo.groupOwnerAddress;
 
             if(wifiP2pInfo.groupFormed && wifiP2pInfo.isGroupOwner) {
-                connectionStatus2.setText("Teacher");
+                connectionStatus.setText("Teacher");
+                serverClass = new ServerClass();
+                serverClass.start();
                 //move to a different activity?
             }
             else {
-                connectionStatus2.setText("Student");
-                //move to a different activity?  //does the connection follow?
+                connectionStatus.setText("Student");
+                clientClass = new ClientClass(groupOwnerAddress);
+                clientClass.start();
+                //move to a different activity?
             }
         }
     };
@@ -223,4 +308,87 @@ public class ConnectActivity extends AppCompatActivity {
         super.onPause();
         unregisterReceiver(mReceiver);
     }
+
+    //video #6 server code
+    public class ServerClass extends Thread {
+        Socket socket;
+        ServerSocket serverSocket;
+
+        @Override
+        public void run() {
+            try{ //creates the socket where we can send messages back and forth.
+                serverSocket = new ServerSocket(8888);
+                socket = serverSocket.accept();
+                sendReceive = new SendReceive(socket);
+                sendReceive.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    //video #8 code
+    private class SendReceive extends Thread{
+        private Socket socket;
+        private InputStream inputStream;
+        private OutputStream outputStream;
+
+        public SendReceive(Socket skt) {
+            socket = skt;
+
+            try {
+                inputStream = socket.getInputStream();
+                outputStream = socket.getOutputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+            byte[] buffer = new byte[1024];
+            int bytes;
+
+            while(socket != null) {
+                try {
+                    bytes = inputStream.read(buffer);
+                    if(bytes > 0 ) {//we have something to read
+                        handler.obtainMessage(MESSAGE_READ, bytes,-1, buffer).sendToTarget();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        public void write(byte[] bytes) {
+
+            try {
+                outputStream.write(bytes);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    //video #7 client code
+    public class ClientClass extends Thread {
+        Socket socket;
+        String hostAdd;
+        public ClientClass(InetAddress hostAddress) {
+            hostAdd = hostAddress.getHostAddress();
+            socket = new Socket();
+        }
+        @Override
+        public void run() {
+            try {
+                socket.connect(new InetSocketAddress(hostAdd, 8888), 500);
+                sendReceive = new SendReceive(socket);
+                sendReceive.start();
+                //future code for s ending and receiving
+            }catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
+
