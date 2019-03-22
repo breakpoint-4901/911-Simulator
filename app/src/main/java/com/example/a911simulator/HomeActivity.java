a05b7d3b80fc46facf1e3cdb25e8866aba6603e7
VallejoAgustin <agustinvallejo@my.unt.edu>
2019-03-21 22:48:10 -0500

    removed settings, added lang. selection, updated home act.
    removed settings activity and corresponding resources/imports, now allow for english and spanish languages across the app, need to make language list dynamic based off current language and maybe expand for more

--- app/src/main/java/com/example/a911simulator/HomeActivity.java
+++ app/src/main/java/com/example/a911simulator/HomeActivity.java
@@ -1,17 +1,27 @@
 package com.example.a911simulator;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Locale;
+
 public class HomeActivity extends AppCompatActivity {
+    final String [] supportedLanguages = {"English","Español"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        loadLocale();
         setContentView(R.layout.activity_home);
 
         //grab buttons and put into variables
@@ -20,7 +30,7 @@ public class HomeActivity extends AppCompatActivity {
         Button surveyButton = findViewById(R.id.surveyButton);
 
         /*
-         * create listeners for starting, settings, and survey button
+         * create listeners for starting, changing languages, and survey buttons
          * each listener opens up a new activity
          * */
 
@@ -34,13 +44,12 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
+        //opens a dialog box that allows the user to change languages
         settingsButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                //creates an intent for the SettingsActivity class
-                Intent settings = new Intent(HomeActivity.this, SettingsActivity.class);
-
-                startActivity(settings);
+                //shows AlertDialog to display list of languages, only one can be selected
+                showChangeLanguageDialog();
             }
         });
 
@@ -53,5 +62,69 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(email);
             }
         });
+    }
+
+    private void showChangeLanguageDialog(){
+
+
+        //create alert dialog and pass in list of languages
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(HomeActivity.this);
+        mBuilder.setTitle(R.string.choose_language);
+
+        mBuilder.setSingleChoiceItems(supportedLanguages, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //set locale
+                if(which == 0){
+                    setLocale("en");
+                }
+                else if(which == 1){
+                    setLocale("es");
+                }
+
+                //recreate view
+                recreate();
+
+                //dismiss alert dialog
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog mDialog = mBuilder.create();
+        //show alert dialog
+        mDialog.show();
+    }
+
+    //sets the locale
+    private void setLocale(String lang) {
+        Locale locale = new Locale(lang);
+        Locale.setDefault(locale);
+
+        //deprecated code but does not require app restart
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+
+        //non-deprecated code but requires app restart and debugging to find why it crashes
+        /*Configuration config = new Configuration();
+        config.setLocale(locale);
+        getApplicationContext().createConfigurationContext(config);*/
+
+        //save data to shared preferences
+        saveLocale(lang);
+    }
+
+    //saves locale language to shared preferences
+    private void saveLocale(String lang){
+        SharedPreferences.Editor editor = getSharedPreferences("Settings", MODE_PRIVATE).edit();
+        editor.putString("My_Lang", lang);
+        editor.apply();
+    }
+
+    //load language saved in shared preferences
+    public void loadLocale(){
+        SharedPreferences preferences = getSharedPreferences("Settings", Activity.MODE_PRIVATE);
+        String language = preferences.getString("My_Lang", "");
+        setLocale(language);
     }
 }
