6ccb187b2af27dd7bfed73f98c41c784eb10cd88
Dakota Lambert <32375609+Dako7a@users.noreply.github.com>
2019-03-28 20:54:47 -0500

    Implemented a permission prompt
    At the start of the app, the permissions manager will prompt the user for any and all required permissions.
    
    Added a dependency/library into both build.gradle files.

--- app/src/main/java/com/example/a911simulator/HomeActivity.java
+++ app/src/main/java/com/example/a911simulator/HomeActivity.java
@@ -1,21 +1,30 @@
 package com.example.a911simulator;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.karan.churi.PermissionManager.PermissionManager;
+
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class HomeActivity extends AppCompatActivity {
     final String [] supportedLanguages = {"English","Español"};
+    PermissionManager permissionManager; //used to requesting/checking permissions
 
 
     @Override
@@ -29,6 +38,25 @@ public class HomeActivity extends AppCompatActivity {
         Button settingsButton = findViewById(R.id.settingsButton);
         Button surveyButton = findViewById(R.id.surveyButton);
 
+
+        //prompt our users to accept the permissions when the app is first started.
+       permissionManager = new PermissionManager() { //declare any override functions if the defaults are not to expectations
+           @Override
+           public void ifCancelledAndCannotRequest(Activity activity) { //user clicked DENIED
+               permissionsAlert(); //popup box which informs user they need to accept the permissions
+           }
+           @Override
+           public void ifCancelledAndCanRequest(Activity activity) { //user clicked no ask later
+            //dialog box informing user the permissions are required
+               permissionsAlert(); //popup box which informs user they need to accept the permissions
+           }
+
+
+       };
+        //this does the process of actually requesting the permissions
+       permissionManager.checkAndRequestPermissions(this);
+
+
         /*
          * create listeners for starting, changing languages, and survey buttons
          * each listener opens up a new activity
@@ -38,9 +66,16 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 //creates an intent for the ConnectActivity class
-                Intent connect = new Intent(HomeActivity.this, ConnectActivity.class);
+                ArrayList<String> denied = permissionManager.getStatus().get(0).denied; //queries a list of denied permissions
+                if( denied.size() == 0) {
+                    Intent connect = new Intent(HomeActivity.this, ConnectActivity.class);
 
-                startActivity(connect);
+                    startActivity(connect);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"Please enable the permissions under app preferences.",Toast.LENGTH_LONG).show();
+                }
+
             }
         });
 
@@ -62,6 +97,42 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(email);
             }
         });
+    }
+
+    private void permissionsAlert() {
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(HomeActivity.this, R.style.DialogTheme);
+        builder1.setMessage("Specific permissions are required to use the simulation. Please enable them in app preferences under system settings.");
+        builder1.setCancelable(true);
+
+        builder1.setPositiveButton(
+                "Okay",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+//        builder1.setNegativeButton(
+//                "No",
+//                new DialogInterface.OnClickListener() {
+//                    public void onClick(DialogInterface dialog, int id) {
+//                        dialog.cancel();
+//                    }
+//                });
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        permissionManager.checkResult(requestCode, permissions, grantResults);
+
+        //useful for logging
+        ArrayList<String> granted = permissionManager.getStatus().get(0).granted; //a container holding all of the granted permissions for this application
+        ArrayList<String> denied = permissionManager.getStatus().get(0).denied; //a container holding all of the granted permissions for this application
+
     }
 
     private void showChangeLanguageDialog(){
